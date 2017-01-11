4a15a131cc060a82c6b462ce5a05fb5a9357b577
Greg King <mrgregking@msn.com>
2017-01-11 08:54:16 +0000

    Update Profile Controller.rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,7 +11,7 @@ def create
    # Ensure We Have the Current User Who is Filling OUt the Form. 
     @user = User.find( params[:user_id] )
     # Create Profile Linked to This specific User. 
-    @profile = @user.build_profile( profile_params)
+    @profile = @user.build_profile(profile_params)
   if @profile.save
       flash[:success] = "Profile Updated!"
       redirect_to user_path(id: params[:user_id] )
@@ -33,17 +33,18 @@ end
     # Retrieve that Users Profile
     @profile = @user.profile
     # Mass Assign Edited Profile Attributes and Save (Update)
-    if @profile.update.attributes(profile_params)
+    if @profile.update_attributes(profile_params)
       flash[:success] = "Profile Updated!"
       # Redirect user to thier Profile Page
     redirect_to user_path( params[:user_id] )
   else
     render action: :edit
     end
+  end
     
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
     end
-  end
-end  
+end
+
